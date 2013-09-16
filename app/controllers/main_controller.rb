ca186d1dee7118097591a31ce15943367744bffd
Long Nguyen <longn2013@gmail.com>
2013-09-17 01:10:20 +0700

    add basic stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,20 @@
 class MainController < ApplicationController
   def index
   end
+
+  def team
+    
+  end
+
+  def about
+    
+  end
+
+  def donggop
+    
+  end
+
+  def library
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 KimquyScreencast::Application.routes.draw do
   devise_for :users
   root :to => "main#index"
+  get "/team" => "main#team"
+  get "/about" => "main#about"
+  get "/donggop" => "main#donggop"
+  get "/library" => "main#library"
 end
