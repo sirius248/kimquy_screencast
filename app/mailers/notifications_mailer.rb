b021ea24d35ca2bbe983ac4841f3f6c0dcc4a3c8
KhanhPham <phamkhanh90@gmail.com>
2013-10-07 20:00:44 +0700

    delete

--- app/mailers/notifications_mailer.rb
+++
@@ -1,9 +0,0 @@
-class NotificationsMailer < ActionMailer::Base
-  default from: "phamkhanh928@gmail.com"
-  default to: "khanhpn@zigexn.vn"
-
-  def new_message(message)
-    @message =  message
-    mail(subject: "[GitTuts] #{message.subject}")
-  end
-end
--- app/models/message.rb
+++
@@ -1,6 +0,0 @@
-class Message < ActiveRecord::Base
-  attr_accessor :email, :subject, :content
-
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates :email, :subject, :content, :presence => true
-end
