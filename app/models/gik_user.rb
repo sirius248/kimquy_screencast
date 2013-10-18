687bb9b9b06c1c5f898db7ab27b8885d3d8ccb56
KhanhPham <phamkhanh90@gmail.com>
2013-10-18 23:28:05 +0700

    fix show error signup

--- app/models/gik_user.rb
+++ app/models/gik_user.rb
@@ -1,10 +1,12 @@
 class GikUser < ActiveRecord::Base
   before_save {self.email = email.downcase}
 
-  validates :name, presence: true, length: {maximum: 80}
+  validates :name, presence: {message: I18n::t("error.gik_user.presence_name")}, length: {maximum: 80, message: I18n::t("error.gik_user.length_name")}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :email, presence: {message: I18n::t("error.gik_user.presence_email")}, format: {with: VALID_EMAIL_REGEX, message: I18n.t("error.gik_user.format_email")
+}, uniqueness: {case_sensitive: false, message: I18n::t("error.gik_user.uniqueness_email")}
 
   has_secure_password
-  validates :password, length: {minimum: 6}
+  validates :password, presence: {message: I18n::t("error.gik_user.presence_password")}, length: {minimum: 6, message: I18n::t("error.gik_user.length_password")}
+  validates :password_confirmation, presence: {message: I18n::t("error.gik_user.presence_password_confirmation")}, length: {minimum: 6, message: I18n::t("error.gik_user.length_password_confirmation")}
 end
